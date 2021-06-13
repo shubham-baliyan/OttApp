2d76cc6f13620aaeb83738cc7c3abf80697b60db
shubham baliyan <shubham.baliyan204@gmail.com>
2021-06-13 11:22:15 +0530

    fixed apollo client erro

--- src/index.js
+++ src/index.js
@@ -6,9 +6,10 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import appConfig from "./config";
 
 //intialise the Apollo Client
+
 const client = new ApolloClient({
-  uri: appConfig.domain,
-  cache: InMemoryCache(),
+  uri: appConfig.graphDomain,
+  cache: new InMemoryCache(),
 });
 
 ReactDOM.render(
