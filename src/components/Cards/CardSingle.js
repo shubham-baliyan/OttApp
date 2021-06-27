ce8aa9624e6e665a3ad4e6cc4164300dde6aff01
shubham-baliyan <shubham.baliyan204@gmail.com>
2021-06-27 21:20:07 +0530

    image loading

--- src/components/Cards/CardSingle.js
+++ src/components/Cards/CardSingle.js
@@ -3,40 +3,41 @@ import { Link } from "react-router-dom";
 import pic from "../../assets/images/pic.webp";
 
 const CardSingle = ({ item }) => {
-
-  const addToLocal=()=>{
-    let items=JSON.parse(localStorage.getItem("watchList"));
+  const addToLocal = () => {
+    let items = JSON.parse(localStorage.getItem("watchList"));
     items.push(item);
-    localStorage.setItem('watchList',JSON.stringify(items))
-  }
+    localStorage.setItem("watchList", JSON.stringify(items));
+  };
   return (
     <Link to={`/single/${item.id}`}>
-
-    <div className="card__container">
-      <div>
-        <img className="card__img" src={item.poster} alt="big bull" />
-      </div>
-      <div className="card__details">
-        <h1 className="card__h1">{item.title}</h1>
-        <h2 className="card__h2">
-          {item.runtime}, <br />
-        </h2>
-        <p className="card__p">{item.tagline}</p>
+      <div className="card__container">
+        <div>
+          <img
+            className="card__img loaderGif"
+            width="180px"
+            height="288px"
+            src={item.poster}
+            alt={item.title}
+          />
+        </div>
+        <div className="card__details">
+          <h1 className="card__h1">{item.title}</h1>
+          <h2 className="card__h2">
+            {item.runtime}, <br />
+          </h2>
+          <p className="card__p">{item.tagline}</p>
           <Link className="card__button text-white" to={`/single/${item.id}`}>
-           
-              <i className="fa fa-play mr-2"></i>
-                Watch now
-            
+            <i className="fa fa-play mr-2"></i>
+            Watch now
           </Link>
-        <button className="card__button" onClick={addToLocal}>
-          <i className="fa fa-plus mr-2"></i>
-          {/* <Link className="card__button--link" to={`/single/${item.id}`}> */}
+          <button className="card__button" onClick={addToLocal}>
+            <i className="fa fa-plus mr-2"></i>
+            {/* <Link className="card__button--link" to={`/single/${item.id}`}> */}
             Add to WatchList
-          {/* </Link> */}
-        </button>
+            {/* </Link> */}
+          </button>
+        </div>
       </div>
-    </div>
-
     </Link>
   );
 };
